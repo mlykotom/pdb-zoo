0cede7985875a544fad88eb1599cb2f358edd908
Jakub Tutko <jakub.tutko@gmail.com>
2015-10-12 11:16:58 +0200

    some comments added

--- src/main/java/controller/LoginFrame/ButtonLogin.java
+++ src/main/java/controller/LoginFrame/ButtonLogin.java
@@ -11,16 +11,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Created by Jakub on 12.10.2015.
+ * ActionListener for the login button in the LoginFrame.
+ * <p>
+ * Button tries to connect database with the given User name and Password.
+ * When connection is successful, LoginFrame is disposed and MenuFrame is
+ * opened. Otherwise application stays on the LoginFrame and error message
+ * is displayed.
+ *
+ * @author Jakub Tutko
  */
 public class ButtonLogin implements ActionListener {
 
 	private LoginFrame frame;
 
+	/**
+	 * Constructor saves instance of the LoginFrame. All changes will be
+	 * applicable on this frame.
+	 *
+	 * @param frame instance of the LoginFrame to work with
+	 */
 	public ButtonLogin(LoginFrame frame) {
 		this.frame = frame;
 	}
 
+	/**
+	 * Action tries connect database, with User name and Password from the
+	 * frame resources. On success, LoginFrame is disposed and MenuFrame is
+	 * set to be visible. On failure, error dialog is displayed.
+	 *
+	 * @param e event, which causes this action
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		DataManager dataManager = DataManager.getInstance();
--- src/main/java/controller/LoginFrame/ButtonQuit.java
+++ src/main/java/controller/LoginFrame/ButtonQuit.java
@@ -6,16 +6,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Created by Jakub on 12.10.2015.
+ * ActionListener for the quit button in LoginFrame.
+ * <p>
+ * Button closes the application.
+ *
+ * @author Jakub Tutko
  */
 public class ButtonQuit implements ActionListener {
 
 	private LoginFrame frame;
 
+	/**
+	 * Constructor saves instance of the frame, in which action
+	 * will be applicable.
+	 *
+	 * @param frame instance of the LoginFrame class
+	 */
 	public ButtonQuit(LoginFrame frame) {
 		this.frame = frame;
 	}
 
+	/**
+	 * Action closes the application with exit code 0.
+	 *
+	 * @param e event, which causes this action
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.exit(0);
