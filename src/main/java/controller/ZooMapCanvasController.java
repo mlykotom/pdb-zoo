3fdf4c11be682b11d0d075cc4bb4443b3a98c9b2
Jakub Tutko <jakub.tutko@gmail.com>
2015-10-17 11:37:49 +0200

    zooMapCanvas created

---
+++ src/main/java/controller/ZooMapCanvasController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import gui.ZooMapCanvas;
+
+/**
+ * Created by Jakub on 15.10.2015.
+ */
+public class ZooMapCanvasController extends Controller {
+	private ZooMapCanvas canvas;
+
+	public ZooMapCanvasController(ZooMapCanvas zooMapCanvas) {
+		super();
+		this.canvas = zooMapCanvas;
+	}
+}
---
+++ src/main/java/gui/zooMapCanvas.java
@@ -0,0 +1,91 @@
+package gui;
+
+import controller.ZooMapCanvasController;
+import controller.ZooMapFormController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Created by Jakub on 15.10.2015.
+ */
+public class ZooMapCanvas extends JPanel {
+	private final ZooMapCanvasController controller;
+
+	public ZooMapCanvas() {
+		this.controller = new ZooMapCanvasController(this);
+		MouseMoveScale();
+	}
+
+	private Rectangle2D.Float myRect = new Rectangle2D.Float(50, 50, 50, 50);
+
+	public MovingAdapter ma = new MovingAdapter();
+
+	public void MouseMoveScale() {
+		addMouseMotionListener(ma);
+		addMouseListener(ma);
+		addMouseWheelListener(new ScaleHandler());
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+
+		Graphics2D g2d = (Graphics2D) g;
+
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+		g2d.setColor(new Color(0, 0, 200));
+		g2d.fill(myRect);
+	}
+
+	class MovingAdapter extends MouseAdapter {
+
+		private int x;
+
+		private int y;
+
+		public void mousePressed(MouseEvent e) {
+			x = e.getX();
+			y = e.getY();
+		}
+
+		public void mouseDragged(MouseEvent e) {
+
+			int dx = e.getX() - x;
+			int dy = e.getY() - y;
+
+			if (myRect.getBounds2D().contains(x, y)) {
+				myRect.x += dx;
+				myRect.y += dy;
+				repaint();
+			}
+			x += dx;
+			y += dy;
+		}
+	}
+
+	class ScaleHandler implements MouseWheelListener {
+		public void mouseWheelMoved(MouseWheelEvent e) {
+
+			int x = e.getX();
+			int y = e.getY();
+
+			if (e.getScrollType() == MouseWheelEvent.WHEEL_UNIT_SCROLL) {
+
+				if (myRect.getBounds2D().contains(x, y)) {
+					float amount = e.getWheelRotation() * 5f;
+					myRect.width += amount;
+					myRect.height += amount;
+					repaint();
+				}
+			}
+		}
+	}
+}
