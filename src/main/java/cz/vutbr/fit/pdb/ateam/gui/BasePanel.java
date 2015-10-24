d562404a555ce725a68b7c770918ee41bd014461
Tomáš Hanus <xhanus11@stud.fit.vutbr.cz>
2015-10-24 17:07:06 +0200

    Added BasePanel

---
+++ src/main/java/cz/vutbr/fit/pdb/ateam/gui/BasePanel.java
@@ -0,0 +1,20 @@
+package cz.vutbr.fit.pdb.ateam.gui;
+
+import cz.vutbr.fit.pdb.ateam.controller.Controller;
+
+import javax.swing.*;
+
+/**
+ * Generic Panel contains abstract class getController to ensure that every Panel will contain a controller.
+ *
+ * @Author Tomas Hanus
+ */
+public abstract class BasePanel extends JPanel {
+
+	/**
+	 * Allows to get to the parent controller
+	 *
+	 * @return Controller for specific Panel
+	 */
+	protected abstract Controller getController();
+}
