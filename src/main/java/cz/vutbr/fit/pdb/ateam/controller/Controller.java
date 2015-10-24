16229768e77503e37e1fc146e655ce9ee6f41fbd
Tomáš Hanus <xhanus11@stud.fit.vutbr.cz>
2015-10-24 19:53:40 +0200

    Removed ContelPanelController Created ContentPanelObserverSubject for managing listeners

--- src/main/java/cz/vutbr/fit/pdb/ateam/controller/Controller.java
+++ src/main/java/cz/vutbr/fit/pdb/ateam/controller/Controller.java
@@ -2,9 +2,6 @@ package cz.vutbr.fit.pdb.ateam.controller;
 
 import cz.vutbr.fit.pdb.ateam.adapter.DataManager;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Class for managing all view events.
  * <p>
@@ -17,24 +14,11 @@ import java.util.List;
  */
 public class Controller {
 	protected DataManager dataManager;
-	private List<ObjectSelectionChangedListener> objectSelectionChangedListeners = new ArrayList<ObjectSelectionChangedListener>();
 
 	/**
 	 * Saves static DataManager instance into local variable.
 	 */
 	protected Controller() {
 		dataManager = DataManager.getInstance();
-	}
-
-
-
-	public interface ObjectSelectionChangedListener{
-		void notifyObjectSelectionChanged();
-	}
-
-	public void notifyAllObjectSelectionChangedListeners(){
-		for (ObjectSelectionChangedListener listener :  this.objectSelectionChangedListeners){
-			listener.notifyObjectSelectionChanged();
-		}
 	}
 }
--- src/main/java/cz/vutbr/fit/pdb/ateam/controller/EmployeesTabController.java
+++ src/main/java/cz/vutbr/fit/pdb/ateam/controller/EmployeesTabController.java
@@ -1,14 +1,22 @@
 package cz.vutbr.fit.pdb.ateam.controller;
 
 import cz.vutbr.fit.pdb.ateam.gui.detail.EmployeesTab;
+import cz.vutbr.fit.pdb.ateam.model.spatial.SpatialObjectModel;
+import cz.vutbr.fit.pdb.ateam.observer.ContentPanelObserverSubject;
 
 /**
  * Created by Tomas on 10/24/2015.
  */
-public class EmployeesTabController extends Controller {
+public class EmployeesTabController extends Controller implements ContentPanelObserverSubject.ObjectSelectionChangedListener {
 	private EmployeesTab panel;
 
 	public EmployeesTabController(EmployeesTab employeesPanel) {
 		this.panel = employeesPanel;
+		ContentPanelObserverSubject.getInstance().subscribeForSelectionChange(this);
+	}
+
+	@Override
+	public void notifyObjectSelectionChanged(SpatialObjectModel spatialObjectModel) {
+		//TODO Do a reacton on NotifyObjectSelectonCHanged()
 	}
 }
