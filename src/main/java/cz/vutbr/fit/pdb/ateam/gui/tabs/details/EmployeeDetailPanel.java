58fb0667aa9155e2e78ff4810116e26337a7e78a
Tomáš Hanus <xhanus11@stud.fit.vutbr.cz>
2015-12-01 19:05:21 +0100

    EmployeeDetailPanel and EmployeesListPanel added

---
+++ src/main/java/cz/vutbr/fit/pdb/ateam/gui/tabs/details/EmployeeDetailPanel.java
@@ -0,0 +1,7 @@
+package cz.vutbr.fit.pdb.ateam.gui.tabs.details;
+
+/**
+ * Created by Tomas on 12/1/2015.
+ */
+public class EmployeeDetailPanel {
+}
---
+++ src/main/java/cz/vutbr/fit/pdb/ateam/gui/tabs/lists/EmployeesListPanel.java
@@ -0,0 +1,7 @@
+package cz.vutbr.fit.pdb.ateam.gui.tabs.lists;
+
+/**
+ * Created by Tomas on 12/1/2015.
+ */
+public class EmployeesListPanel {
+}
