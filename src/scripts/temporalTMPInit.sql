64675df297b0e301dc700ac2abb956c5509dbbff
Tomáš Hanus <xhanus11@stud.fit.vutbr.cz>
2015-12-08 21:16:59 +0100

    temporalScriptInit storedProcedure for update progress started

--- src/scripts/temporalTMPInit.sql
+++ src/scripts/temporalTMPInit.sql
@@ -77,3 +77,55 @@ SELECT * FROM Employees_Shift
 SELECT e.ID, e.Name, e.Surname, Location FROM EMPLOYEES e JOIN Employees_Shift s ON e.ID = s.EmplId
 
 SELECT * FROM Employees_Shift
+
+
+--CREATE OR REPLACE PROCEDURE updateEmployeeTable(dateFrom IN Date, dateTo IN Date, employeeID IN LONG, location in LONG ) IS
+CREATE OR REPLACE PROCEDURE updateEmployeeTable(dateFrom IN Date, dateTo IN Date, employeeID IN LONG) IS
+  t_compl_in VARCHAR(255);
+  t_right_in VARCHAR(255);
+  t_left_in VARCHAR(255);
+
+  BEGIN
+    FOR rec IN (SELECT * FROM EMPLOYEES_SHIFT sh WHERE sh.EmplID = 28)
+    LOOP
+
+      IF (rec.DFROM >= dateFrom AND rec.DTO <= dateTo) THEN
+        INSERT INTO Employees_ShiftTemporal (ID, EmplID, Location, dFrom, dTo)
+        VALUES (rec.ID, rec.EMPLID, rec.LOCATION, rec.DFROM, rec.DTO);
+      END IF;
+    END LOOP;
+
+  END;
+
+
+
+INSERT INTO Employees_Shift ( EmplId, Location, dFrom, dTo ) VALUES (28, 2, '01-Jan-1995', '01-Jan-1998');
+
+INSERT INTO Employees_Shift ( EmplId, Location, dFrom, dTo ) VALUES (28, 2, '01-Jan-1990', '01-Jan-1993');
+
+CREATE TABLE Employees_ShiftTemporal (
+  ID INT NOT NULL,
+  EmplID INT NOT NULL,
+  Location INT,
+  dFrom DATE NOT NULL,
+  dTo DATE NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (Location) REFERENCES Spatial_Objects (ID),
+  FOREIGN KEY (EmplID) REFERENCES Employees (ID)
+);
+
+DECLARE
+  v_Date1 date := to_date('01-JAN-1998', 'DD-Mon-YYYY');
+BEGIN
+  updateEmployeeTable(v_Date1, 28);
+END;
+
+commit
+
+DELETE Employees_ShiftTemporal;
+
+SELECT * FROM Employees_ShiftTemporal
+
+
+
+
